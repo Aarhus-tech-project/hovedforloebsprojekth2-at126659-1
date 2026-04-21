16cf0030dda8fa7e8dd1ab2e1fce929ecff38aac
lauri <at126659@edu.aarhustech.dk>
2026-04-21 13:52:18 +0200

    .

--- gamedata.Api/Services/UserService.cs
+++ gamedata.Api/Services/UserService.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using User.Models;
 
 namespace User.Services
@@ -9,12 +11,18 @@ namespace User.Services
         {
             Users = new List<AppUser>
             {
-                new AppUser { Id = 1, Username = "JohnDoe", Avatar = null, Email = null, Password = "password123" },
-                new AppUser { Id = 2, Username = "JaneSmith", Avatar = null, Email = null, Password = "password456" }
+                new AppUser { Id = 1, CreatedAt = DateTime.UtcNow, Username = "JohnDoe", Avatar = null, Password = "password123" },
+                new AppUser { Id = 2, CreatedAt = DateTime.UtcNow, Username = "JaneSmith", Avatar = null, Password = "password456" }
             };
         }
         public static List<AppUser> GetAll() => Users;
         public static AppUser? Get(int id) => Users.FirstOrDefault(u => u.Id == id);
+        public static AppUser? Post(string username, int? avatar, string password)
+        {
+            var user = new AppUser { Id = Users.Count + 1, CreatedAt = DateTime.UtcNow, Username = username, Avatar = avatar, Password = password };
+            Users.Add(user);
+            return user;
+        }
     }
     
     // public interface IUserService

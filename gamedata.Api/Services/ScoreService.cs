eb73efaaf49d609ec1d780f9d8e9437f95b22e50
lauri <at126659@edu.aarhustech.dk>
2026-04-22 09:18:17 +0200

    Found better way to set date in placeholder data

--- gamedata.Api/Services/ScoreService.cs
+++ gamedata.Api/Services/ScoreService.cs
@@ -11,23 +11,23 @@ namespace Score.Services
         {
             Scores = new List<GameScore>
             {
-                new GameScore { ScoreId = 1, UserId = 1, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(67), Game_Score = 6, Game_Version = "1.0.0" },
-                new GameScore { ScoreId = 2, UserId = 1, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(12), Game_Score = 5, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 1, UserId = 1, CreatedAt = DateTime.UtcNow.AddDays(-67), Game_Score = 6, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 2, UserId = 1, CreatedAt = DateTime.UtcNow.AddDays(-12), Game_Score = 5, Game_Version = "1.0.0" },
                 new GameScore { ScoreId = 3, UserId = 1, CreatedAt = DateTime.UtcNow, Game_Score = 5, Game_Version = "1.0.1" },
                 new GameScore { ScoreId = 4, UserId = 1, CreatedAt = DateTime.UtcNow, Game_Score = 6, Game_Version = "1.0.1" },
-                new GameScore { ScoreId = 5, UserId = 2, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(6), Game_Score = 7, Game_Version = "1.0.0" },
-                new GameScore { ScoreId = 6, UserId = 2, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(2), Game_Score = 4, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 5, UserId = 2, CreatedAt = DateTime.UtcNow.AddDays(-6), Game_Score = 7, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 6, UserId = 2, CreatedAt = DateTime.UtcNow.AddDays(-2), Game_Score = 4, Game_Version = "1.0.0" },
                 new GameScore { ScoreId = 7, UserId = 2, CreatedAt = DateTime.UtcNow, Game_Score = 9, Game_Version = "1.0.1" },
                 new GameScore { ScoreId = 8, UserId = 2, CreatedAt = DateTime.UtcNow, Game_Score = 7, Game_Version = "1.0.2" },
-                new GameScore { ScoreId = 9, UserId = 3, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(29), Game_Score = 10, Game_Version = "1.0.0" },
-                new GameScore { ScoreId = 10, UserId = 3, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(9), Game_Score = 8, Game_Version = "1.0.1" },
+                new GameScore { ScoreId = 9, UserId = 3, CreatedAt = DateTime.UtcNow.AddDays(-29), Game_Score = 10, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 10, UserId = 3, CreatedAt = DateTime.UtcNow.AddDays(-9), Game_Score = 8, Game_Version = "1.0.1" },
                 new GameScore { ScoreId = 11, UserId = 3, CreatedAt = DateTime.UtcNow, Game_Score = 6, Game_Version = "1.0.2" },
-                new GameScore { ScoreId = 12, UserId = 4, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(1), Game_Score = 4, Game_Version = "1.0.2" },
+                new GameScore { ScoreId = 12, UserId = 4, CreatedAt = DateTime.UtcNow.AddDays(-1), Game_Score = 4, Game_Version = "1.0.2" },
                 new GameScore { ScoreId = 13, UserId = 4, CreatedAt = DateTime.UtcNow, Game_Score = 9, Game_Version = "1.0.2" },
                 new GameScore { ScoreId = 14, UserId = 4, CreatedAt = DateTime.UtcNow, Game_Score = 8, Game_Version = "1.0.2" },
-                new GameScore { ScoreId = 15, UserId = 5, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(17), Game_Score = 10, Game_Version = "1.0.0" },
-                new GameScore { ScoreId = 16, UserId = 5, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(11), Game_Score = 9, Game_Version = "1.0.0" },
-                new GameScore { ScoreId = 17, UserId = 5, CreatedAt = DateTime.UtcNow - TimeSpan.FromDays(11), Game_Score = 11, Game_Version = "1.0.0" }
+                new GameScore { ScoreId = 15, UserId = 5, CreatedAt = DateTime.UtcNow.AddDays(-17), Game_Score = 10, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 16, UserId = 5, CreatedAt = DateTime.UtcNow.AddDays(-11), Game_Score = 9, Game_Version = "1.0.0" },
+                new GameScore { ScoreId = 17, UserId = 5, CreatedAt = DateTime.UtcNow.AddDays(-11), Game_Score = 11, Game_Version = "1.0.0" }
             };
         }
         public static List<GameScore> GetAll() => Scores;

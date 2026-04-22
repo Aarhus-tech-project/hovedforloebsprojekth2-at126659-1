b6db639377f402f70a6826b7c55f7c2519748782
lauri <at126659@edu.aarhustech.dk>
2026-04-22 08:55:46 +0200

    Added more placeholder data for testing

--- gamedata.Api/Services/UserService.cs
+++ gamedata.Api/Services/UserService.cs
@@ -12,7 +12,10 @@ namespace User.Services
             Users = new List<AppUser>
             {
                 new AppUser { Id = 1, CreatedAt = DateTime.UtcNow, Username = "JohnDoe", Avatar = null, Password = "password123" },
-                new AppUser { Id = 2, CreatedAt = DateTime.UtcNow, Username = "JaneSmith", Avatar = null, Password = "password456" }
+                new AppUser { Id = 2, CreatedAt = DateTime.UtcNow, Username = "JaneSmith", Avatar = null, Password = "password456" },
+                new AppUser { Id = 3, CreatedAt = DateTime.UtcNow, Username = "PlayerOne", Avatar = null, Password = "password789" },
+                new AppUser { Id = 4, CreatedAt = DateTime.UtcNow, Username = "GamerGirl", Avatar = null, Password = "passwordabc" },
+                new AppUser { Id = 5, CreatedAt = DateTime.UtcNow, Username = "ProGamer", Avatar = null, Password = "passworddef" }
             };
         }
         public static List<AppUser> GetAll() => Users;
